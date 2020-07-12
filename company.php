c559a7ea759f17045d9280f9da6c7a22579f8f5b
EL IDRISSI Chafik <chafik.elidrissi@outlook.fr>
2020-07-12 21:09:43 +0200

    Update company.php
    Adding ';' on include line 5

--- company.php
+++ company.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include './connect.php'
+include './connect.php';
 include './request.php';
 ?>
 <!doctype html>
